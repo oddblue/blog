562ca787ca27fe43b23a33cfd1b20bd3247711f3
oddblue <1850528268@qq.com>
2025-03-13 16:51:33 +0800

    new file:   .specstory/history/.what-is-this.md 	new file:   blog/.gitignore 	new file:   blog/.vscode/extensions.json 	new file:   blog/README.md 	new file:   blog/index.html 	new file:   blog/package-lock.json 	new file:   blog/package.json 	new file:   blog/public/vite.svg 	new file:   blog/src/App.vue 	new file:   blog/src/assets/vue.svg 	new file:   blog/src/main.js 	new file:   blog/src/router/index.js 	new file:   blog/src/style.css 	new file:   blog/src/views/Home.vue 	new file:   blog/vite.config.js

---
+++ blog/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Home from './views/Home.vue';
+</script>
+
+<template>
+  <div id="app">
+    <!-- 路由导航 -->
+    <!-- 路由出口 -->
+    <router-view></router-view>
+  </div>
+</template>
---
+++ blog/src/views/Home.vue
@@ -0,0 +1,26 @@
+<template lang="">
+    <div class='container'>
+        <div>首页</div>
+        <div>
+            <div>
+                <div>笔记</div>
+                <div>八股文</div>
+                <div>封装函数</div>
+                <div>网址导航</div>
+            </div>
+            <div>深色模式</div>
+            <div>git</div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped lang="">
+.container{
+    display:fixed
+    
+}
+</style>
